3da7626097246dd287f8cc75b037cd79353f07bb
Sai Chandana <125032786+SaiChandana-collab@users.noreply.github.com>
2024-08-13 15:23:21 +0530

    Update app.js
    Updated the API URL and corresponding changes are done as the old one is not working.

--- CurrencyConverter/app.js
+++ CurrencyConverter/app.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+  "https://v6.exchangerate-api.com/v6/9e24d231ad236041a27fa837/latest";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -32,10 +32,10 @@ const updateExchangeRate = async () => {
     amtVal = 1;
     amount.value = "1";
   }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  const URL = `${BASE_URL}/${fromCurr.value}`;
   let response = await fetch(URL);
   let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
+  let rate = data["conversion_rates"][toCurr.value];
 
   let finalAmount = amtVal * rate;
   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
