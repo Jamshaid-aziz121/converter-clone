421ed70fed04fd055f67b0c9263dab8bcdf80b87
mukthar <138196754+Umar-Mukthar-Ahmed@users.noreply.github.com>
2024-09-26 22:04:29 +0530

    Resolved

--- CurrencyConverter/app.js
+++ CurrencyConverter/app.js
@@ -1,6 +1,3 @@
-// const BASE_URL =
-//   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
 const BASE_URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
 
